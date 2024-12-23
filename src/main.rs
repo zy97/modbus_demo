1c43a506e5e159ed0947a78d3669011402711ed5
bomky <bom_ky@outlook.com>
2024-12-23 14:48:26 +0800

    移除一些没用的文件

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, middleware, web, App, HttpServer, Responder};
+use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, FmtSubscriber};
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
@@ -7,8 +8,24 @@ async fn greet(name: web::Path<String>) -> impl Responder {
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(greet))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    let file_appender = tracing_appender::rolling::daily("logs", "app.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+    // 创建一个文件输出层
+    let file_layer = Layer::new().with_writer(non_blocking); // 输出到文件
+
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(tracing::Level::INFO)
+        .finish()
+        .with(file_layer);
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    HttpServer::new(|| {
+        App::new()
+            .service(greet)
+            .wrap(middleware::Logger::default())
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
