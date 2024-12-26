51ecfcc38c73a697ae593676419fed332091f60f
bomky <bom_ky@outlook.com>
2024-12-26 10:26:21 +0800

    更新日志

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,18 @@ use app_config::{load_config, AppConfig};
 use modbus_manager::{ModbusContext, Pool};
 use opentelemetry::global;
 use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
+use opentelemetry_sdk::logs::LoggerProvider;
 use opentelemetry_sdk::propagation::TraceContextPropagator;
 use otlp::{init_logs, init_traces};
 use server_router::{get_modbus_value, greet};
+use std::fmt::Error;
 use std::{collections::HashMap, sync::LazyLock};
 use tracing::{debug, info};
 use tracing_subscriber::fmt;
+use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::EnvFilter;
-use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, FmtSubscriber};
 static APP_CONFIG: LazyLock<AppConfig> = LazyLock::new(|| {
-    // M3 Ultra takes about 16 million years in --release config
     let config = load_config().unwrap();
     debug!("加载配置成功：{:#?}", config);
     config
@@ -26,7 +27,39 @@ static APP_CONFIG: LazyLock<AppConfig> = LazyLock::new(|| {
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     global::set_text_map_propagator(TraceContextPropagator::new());
-    let logger_provider = init_logs().unwrap();
+    let logger_provider = init_log().unwrap();
+    let pools: HashMap<String, Pool> = APP_CONFIG
+        .modbus
+        .configs
+        .iter()
+        .map(|config| {
+            let mgr = ModbusContext {
+                addr: config.address.to_string(),
+                slave: config.slave_id,
+            };
+            let modbus_pool = Pool::builder(mgr).max_size(50).build().unwrap();
+            (config.name.clone(), modbus_pool)
+        })
+        .collect();
+    let server_url = &*APP_CONFIG.server.address;
+    info!(name: "my-event", target: "my-target", "hello from {}. My price is {}", "apple", 1.99);
+    let tracer_provider = init_traces().unwrap();
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pools.clone()))
+            .service(greet)
+            .service(get_modbus_value)
+            .wrap(middleware::Logger::default())
+    })
+    .bind(server_url)?
+    .run()
+    .await?;
+    global::set_tracer_provider(tracer_provider);
+    logger_provider.shutdown().unwrap();
+    Ok(())
+}
+fn init_log() -> Result<LoggerProvider, Error> {
+    let logger_provider: opentelemetry_sdk::logs::LoggerProvider = init_logs().unwrap();
     let otel_layer = OpenTelemetryTracingBridge::new(&logger_provider);
     let filter_otel = EnvFilter::new("info")
         .add_directive("hyper=off".parse().unwrap())
@@ -44,50 +77,15 @@ async fn main() -> std::io::Result<()> {
     let file_appender = tracing_appender::rolling::daily("logs", "app.log");
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
     // 创建一个文件输出层
-    // let file_layer = Layer::new().with_writer(non_blocking); // 输出到文件
     let file_layer = fmt::layer()
+        .with_ansi(false)
         .with_writer(non_blocking)
         .with_filter(EnvFilter::new("info"));
-    // let subscriber = FmtSubscriber::builder()
-    //     .with_max_level(tracing::Level::DEBUG)
-    //     .finish()
-    //     .with(file_layer);
 
     tracing_subscriber::registry()
         .with(otel_layer)
         .with(fmt_layer)
         .with(file_layer)
         .init();
-    // tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-
-    let pools: HashMap<String, Pool> = APP_CONFIG
-        .modbus
-        .configs
-        .iter()
-        .map(|config| {
-            let mgr = ModbusContext {
-                addr: config.address.to_string(),
-                slave: config.slave_id,
-            };
-            let modbus_pool = Pool::builder(mgr).max_size(50).build().unwrap();
-            (config.name.clone(), modbus_pool)
-        })
-        .collect();
-    let server_url = &*APP_CONFIG.server.address;
-    info!("1");
-    info!(name: "my-event", target: "my-target", "hello from {}. My price is {}", "apple", 1.99);
-    let tracer_provider = init_traces().unwrap();
-    HttpServer::new(move || {
-        App::new()
-            .app_data(web::Data::new(pools.clone()))
-            .service(greet)
-            .service(get_modbus_value)
-            .wrap(middleware::Logger::default())
-    })
-    .bind(server_url)?
-    .run()
-    .await?;
-    global::set_tracer_provider(tracer_provider);
-    logger_provider.shutdown().unwrap();
-    Ok(())
+    Ok(logger_provider)
 }
